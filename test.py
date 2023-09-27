b24d1c013f58b5ca52210ebd19d17719af393899
Valhalla999 <141852125+Valhalla999@users.noreply.github.com>
2023-09-27 20:43:48 +0800

    added Varialbe for color collsion fixet for the player and enemie

--- test.py
+++ test.py
@@ -11,11 +11,16 @@ pygame.init()
 pygame.display.set_caption("Pixie Racer")
 
 #Main Variables
-size = width, height = (1024,768)
+size = width, height = (1024, 768)
 speed = 3
 FPS = 60
 Player_car = "GreenCar.png"
+crash = False
 
+#Colours
+grass_green = (30,180,15)
+street_gray = (50,50,50)
+boarder_white = (255,255,255)
 
 window = pygame.display.set_mode((size))
 
@@ -103,7 +108,7 @@ class Assets:
     
     def asset_draw(self):
         # Load the image from the assets folder
-        self.image = pygame.image.load(join("assets",self.name))
+        self.image = pygame.image.load(join("assets",self.name)).convert_alpha()
         # transform the object
         self.image = pygame.transform.scale(self.image,(self.transform))
         #Get the rect object of the image
@@ -179,17 +184,18 @@ class Labels:
 
 
 class enemie:
-    def __init__(self,name,transform, draw_offset):
+    def __init__(self,name,transform, draw_offset,crash):
         self.name = name
         if random.randint(0,1) == 0:
             self.center = left_lane, draw_offset
         else:
             self.center = right_lane, draw_offset
         self.transform = transform
+        self.crash = crash
     
     def enemie_draw(self):
         # Load the image from the assets folder
-        self.image = pygame.image.load(join("enemies",self.name))
+        self.image = pygame.image.load(join("enemies",self.name)).convert_alpha()
         # transform the object
         self.image = pygame.transform.scale(self.image,(self.transform))
         #Get the rect object of the image
@@ -204,6 +210,7 @@ class enemie:
     def enemie_movement(self):
         self.loc [1] += speed
         if self.loc[1] > height: 
+            global crash
             crash = False          
             if random.randint(0,1) == 0:
                 self.loc.center = right_lane, random.randint (-150, -10)
@@ -215,15 +222,18 @@ class enemie:
 #defining the offset
 asset_main_offset = 0
 
+
 #defining the Assets
+
 bush_amount = 0
 bushes = []
 
 while bush_amount < 10:
-   bush_name = "bush" + str(bush_amount + 1)
-   bushes.append(Assets("Bush1.png", (bush_w, bush_h), asset_main_offset))
-   asset_list.append(bushes[bush_amount])
-   bush_amount += 1
+    bush_name = "bush" + str(bush_amount + 1)
+    bushes.append(Assets("Bush1.png", (bush_w, bush_h), asset_main_offset))
+    asset_list.append(bushes[bush_amount])
+    bush_amount += 1
+
 #defining treeBig assets
 tree_amount = 0
 trees = []
@@ -239,7 +249,7 @@ player1 = Player(Player_car, height -100, (car_w,car_h))
 player1.player_draw()
 
 #define enemie
-enemiecar1 = enemie("RedCar.png", (car_w,car_h), -200)
+enemiecar1 = enemie("RedCar.png", (car_w,car_h), -200, crash)
 enemiecar1.enemie_draw()
 
 #random draw the assets
@@ -247,10 +257,12 @@ for i in range(len(asset_list)):
     asset_list[i].asset_draw()
 
 #define the Background     
-grass_background = Background((30,180,15),0,0,width,height)
-street_background =Background((50,50,50),width/2-road_w/2,0,road_w,height )
-right_boardermarking = Background((255,255,255), width/2 + road_w/2 - roadmark_w * 3, 0, roadmark_w, height )
-left_boardermarking = Background((255,255,255), width/2 - road_w/2 + roadmark_w * 2, 0, roadmark_w, height)
+grass_background = Background((grass_green),0,0,width,height)
+street_background =Background((street_gray),width/2-road_w/2,0,road_w,height )
+right_boardermarking = Background((boarder_white), width/2 + road_w/2 - roadmark_w * 3, 0, roadmark_w, height )
+left_boardermarking = Background((boarder_white), width/2 - road_w/2 + roadmark_w * 2, 0, roadmark_w, height)
+
+
 
 #middle line
 middle = middle_line("Flag.png", (car_w, car_h), -50)
@@ -259,7 +271,7 @@ middle.middle_line_draw()
 counter = 0 
 run = True
 clock = pygame.time.Clock()
-crash = False
+
 
 while run:
     
@@ -269,15 +281,14 @@ while run:
         counter = 0
         print("Level Up", speed)
     
-
-
     #Base Data
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == QUIT:
             run = False
             break 
-
+    
+    #Collision Player/Enemiecar
     if player1.mask.overlap(enemiecar1.mask, (enemiecar1.loc.x  - player1.loc.x , enemiecar1.loc.y - player1.loc.y)):
         if crash == False:
             print("Crash")
@@ -315,7 +326,7 @@ while run:
     speed_lbl.label_draw()
     counter_lbl.label_draw()
 
-
+    
     pygame.display.update()
     
 pygame.quit
